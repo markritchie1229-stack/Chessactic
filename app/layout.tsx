f043a97649c3c1b98c7cecb9843bc7e33c158107
Mark Ritchie <markritchie@marks-air-3.mynetworksettings.com>
2026-07-11 04:34:03 -0400

    Remove ClosedAccountGuard import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { ClosedAccountGuard } from "./components/ClosedAccountGuard";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
